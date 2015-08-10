7531d0ef94fa13c3e0337f406e9b6ae175b33a81
Edward Schults <edward.schults@gmail.com>
2015-08-10 11:02:22 +0200

    added js to resize map on DOM load

--- app/assets/javascripts/app/lines.js
+++ app/assets/javascripts/app/lines.js
@@ -5,3 +5,10 @@ $('.close-white').on('click', function() {
 $('#action-timer').on('click', function() {
   $(this).parent().submit();
 })
+
+$(document).ready(function() {
+  if($('#map').length > 0) {
+    var mapHeight = $(window).height() - $('nav').height() - 20 - $('.search').height() - $('footer').height();
+    $('#map').height(mapHeight);
+  }
+})
